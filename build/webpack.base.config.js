1f3d444ef41cfd9570171142363359420780170c
Diego Vizhnay <diego@vzhny.com>
2019-01-18 11:38:38 -0500

    Added optional chaining babel plugin | 01/18

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -44,7 +44,10 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-optional-chaining',
+            ],
           },
         },
       },
