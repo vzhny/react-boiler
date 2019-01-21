636d100a599b5019f596f2be4a406f70755f941d
Diego Vizhnay <diego@vzhny.com>
2019-01-20 19:28:25 -0500

    Added clean-webpack-plugin -- removed rm -rf ./dist/ from the build script | 01/20

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index',
@@ -33,6 +34,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'app.[contenthash:8].css',
+    }),
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '..'),
     }),
   ],
   module: {
