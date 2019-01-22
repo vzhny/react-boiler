7fbc9bc4cf79fd4b0010c5577a824e4ddcb9900b
Diego Vizhnay <diego@vzhny.com>
2019-01-22 18:02:14 -0500

    Added the ability to access the dev server externally on the network | 01/22

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 const merge = require('webpack-merge');
 const chalk = require('chalk');
+const ip = require('ip');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const webpackBaseConfig = require('./webpack.base.config.js');
 
 const url = chalk.bold(chalk.blue('http://localhost:8080'));
+const networkIP = chalk.bold(chalk.blue(`http://${ip.address()}:8080`));
 const sigInt = chalk.bold(chalk.white('Ctrl + C'));
 
 module.exports = merge(webpackBaseConfig, {
@@ -13,11 +15,16 @@ module.exports = merge(webpackBaseConfig, {
     historyApiFallback: true,
     open: true,
     quiet: true,
+    host: '0.0.0.0',
+    public: 'localhost:8080',
   },
   plugins: [
     new FriendlyErrorsWebpackPlugin({
       compilationSuccessInfo: {
-        messages: [`Dev server is successfully running on ${url}!`],
+        messages: [
+          `Dev server is successfully running on ${url}!`,
+          `Access the dev server externally on ${networkIP}!`,
+        ],
         notes: [`Press ${sigInt} to stop the server.`],
       },
     }),
