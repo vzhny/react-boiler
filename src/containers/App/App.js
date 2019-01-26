40b34f944bd6cec2f49a1b59d5a8eb584352f5e4
Diego Vizhnay <diego@vzhny.com>
2019-01-26 14:29:45 -0500

    Refactored App.js -- now similar to create-react-app | 01/26

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router } from '@reach/router';
 import Navigation from '@/components/Navigation/Navigation';
 import NotFound from '@/pages/NotFound';
+import ReactLogo from '@/images/react_logo.svg';
 import styles from './App.module.scss';
 
 const Home = () => <h1 className={styles.Heading}>Hello World</h1>;
@@ -12,6 +13,7 @@ export default class App extends Component {
   render() {
     return (
       <div className={styles.App}>
+        <img className={styles.Image} src={ReactLogo} alt="React Logo" />
         <Navigation />
         <Router>
           <Home path="/" />
