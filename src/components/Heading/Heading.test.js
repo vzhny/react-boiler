818e3f90529c95f9d82de0b36c0829fb8f168350
Diego Vizhnay <diego@vzhny.com>
2019-01-26 18:09:29 -0500

    Added husky to utilize testing before pushing and committing | 01/26

--- src/components/Heading/Heading.test.js
+++ src/components/Heading/Heading.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 import Heading from './Heading';
 
 /* Followed the guide at https://link.medium.com/fhDZqcKLNT */
@@ -8,13 +8,9 @@ afterEach(cleanup);
 
 describe('Heading', () => {
   it('should contain a message', () => {
-    const { getByText } = render(<Heading message="Hello!" />);
-    getByText('Hello!');
-  });
-
-  it('should click on text', () => {
-    const { getByText } = render(<Heading message="World!" />);
-    const textEl = getByText('World!');
-    fireEvent.click(textEl);
+    const { getByText, rerender } = render(<Heading message="Hello" />);
+    getByText('Hello');
+    rerender(<Heading message="World!" />);
+    getByText('World!');
   });
 });
