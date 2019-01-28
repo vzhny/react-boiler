4a394c1148cf76940bc3266afdd79354ba2ff861
Diego Vizhnay <diego@vzhny.com>
2019-01-27 23:44:49 -0500

    Replaced normalize.css with modern-normalize.css | 01/27

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,10 +5,14 @@ import styles from './Navigation.module.scss';
 const Navigation = () => (
   <ul className={styles.Nav}>
     <li>
-      <Link to="/">Home</Link>
+      <Link className={styles.NavLink} to="/">
+        Home
+      </Link>
     </li>
     <li>
-      <Link to="/about">About</Link>
+      <Link className={styles.NavLink} to="/about">
+        About
+      </Link>
     </li>
   </ul>
 );
