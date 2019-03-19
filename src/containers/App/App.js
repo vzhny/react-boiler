f93e1a649b59f9b9d0ba573e2affccefe4b8d29c
Diego Vizhnay <diego@vzhny.com>
2019-03-19 12:56:55 -0400

    🔨 Updated App component to use newly created page components | 03/19

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import Navigation from '@/components/Navigation/Navigation';
-import Heading from '@/components/Heading/Heading';
-import NotFound from '@/pages/NotFound';
 import ReactLogo from '@/images/react_logo.svg';
+import Landing from '@/pages/Landing';
+import About from '@/pages/About';
+import NotFound from '@/pages/NotFound';
 import { app, image } from './App.module.scss';
 
 const App = () => (
   <div className={app}>
-    <img className={image} src={ReactLogo} alt="React Logo" />
+    <img alt="React Logo" className={image} src={ReactLogo} />
     <Navigation />
     <Router>
-      <Heading message="Hello World!" path="/" />
-      <Heading message="About Us" path="/about" />
+      <Landing path="/" />
+      <About path="/about" />
       <NotFound default />
     </Router>
   </div>
