90f60faa1226eb8a2804f304383d1ab1242b051d
Diego Vizhnay <diego@vzhny.com>
2019-03-19 12:55:48 -0400

    📝 Created  two new pages for landing and about routes | 03/19

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Heading from '@/components/Heading/Heading';
+
+const About = () => {
+  return <Heading message="About Us" />;
+};
+
+export default About;
---
+++ src/pages/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Heading from '@/components/Heading/Heading';
+
+const Landing = () => {
+  return <Heading message="Hello World" />;
+};
+
+export default Landing;
