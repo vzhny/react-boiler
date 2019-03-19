276a379b488f610a66efee13e61dbb9e8f48a972
Diego Vizhnay <diego@vzhny.com>
2019-03-19 12:54:37 -0400

    📝 Added @babel/plugin-proposal-do-expressions | 03/19

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -44,6 +44,7 @@ module.exports = {
             plugins: [
               '@babel/plugin-proposal-class-properties',
               '@babel/plugin-proposal-optional-chaining',
+              '@babel/plugin-proposal-do-expressions',
             ],
           },
         },
