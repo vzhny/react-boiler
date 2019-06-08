4f726f8ea43e2fa45ea15a417d5d35cb60a078a9
Diego Vizhnay <diego@vzhny.com>
2019-06-08 01:37:32 -0400

    ➕ Updated dependencies to latest | 06/08

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -40,7 +40,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [[
+                '@babel/preset-env',
+                {
+                  targets: {
+                    node: 'current',
+                  },
+                },
+              ], '@babel/preset-react'],
             plugins: [
               '@babel/plugin-proposal-class-properties',
               '@babel/plugin-proposal-optional-chaining',
